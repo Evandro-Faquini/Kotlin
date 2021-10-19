210b7165eecb8c89f05ebd4ca3713638a08468cc
Evandro-Faquini <evandrofaquini1981@gmail.com>
2021-10-19 17:14:16 -0300

    Introdução Enum Client e Type

--- src/main/testes/TesteClienteTipoEnum.kt
+++ src/main/testes/TesteClienteTipoEnum.kt
@@ -3,13 +3,20 @@ import main.ClienteTipo
 fun main() {
 
     ClienteTipo.values().forEach { elemento ->
-        println(" ${elemento.name} - $elemento")
+        println("${elemento.name} - ${elemento.Descricao}")
 
     }
 
     ClienteTipo.values().forEach {
-        println("${it.name} - $it")
+        println("${it.name} - ${it.Descricao}")
     }
+
+    val pf = ClienteTipo.PF
+    println("${pf.name} - ${pf.Descricao}")
+
+    val pj = ClienteTipo.PJ
+    println("${pj.name} - ${pj.Descricao}")
+
 }
 
 
